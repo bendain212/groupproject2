765a4d10c75615ba216b3f0311983975101badfe
cpooniwala <cpooniwala@gmail.com>
2020-03-29 17:23:43 -0500

    Added SQL database and created a model for media

---
+++ models/media.js
@@ -0,0 +1,25 @@
+module.exports = function(sequelize, DataTypes) {
+  var Media = sequelize.define("Media", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1]
+    }
+  });
+
+  Media.associate = function(models) {
+    Media.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+  return Media;
+};
