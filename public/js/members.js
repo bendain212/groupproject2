43b88a315b4a0a0ab366537594afc4d1fd68aebd
cpooniwala <cpooniwala@gmail.com>
2020-04-01 19:31:26 -0500

    Dynamically generated paypal button

--- public/js/members.js
+++ public/js/members.js
@@ -17,7 +17,14 @@ $(document).ready(function() {
           ${data[i].categorization}
           </p>
           <p class="card-text">
-          ${paypalUsername}
+          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
+            <input type="hidden" name="cmd" value="_donations" />
+            <input type="hidden" name="business" value="${paypalUsername}" />
+            <input type="hidden" name="currency_code" value="USD" />
+            <input type="image" src="https://www.paypalobjects.com/webstatic/en_US/i/buttons/PP_logo_h_100x26.png" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+            <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+        </form>
+          
           </p>
           <a href=${data[i].youtubeURL} class="btn btn-primary">
             Youtube Link
